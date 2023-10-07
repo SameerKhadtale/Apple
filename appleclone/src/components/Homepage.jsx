27c6b3fdf02b55dc9f8683d9040c3dbcd71e5c8f
yash-gole28 <yashgole2806@gmail.com>
2023-10-07 17:55:38 +0530

    updates

---
+++ appleclone/src/components/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+import HomeContent from './HomeContent'
+const Homepage = () => {
+  return (
+    <div id='screen'>
+      <Navbar/>
+      <HomeContent/>
+    </div>
+  )
+}
+
+export default Homepage
---
+++ appleclone/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <ul>
+            <li><i class="fa-brands fa-apple"></i></li>
+            <li>Store</li>
+            <li>Mac</li>
+            <li>iPad</li>
+            <li>iPhone</li>
+            <li>Watch</li>
+            <li>Vision</li>
+            <li>AirPods</li>
+            <li>TV & Home</li>
+            <li>Entertainment</li>
+            <li>Accessories</li>
+            <li>Support</li>
+            <li><i class="fa-solid fa-magnifying-glass"></i></li>
+            <li><i class="fa-solid fa-cart-shopping"></i></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
